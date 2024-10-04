343e14cefc1be3e73923326efe9c93efb59f59b1
khantanvir <aiub.tanvir@gmail.com>
2024-10-05 01:54:51 +0600

    change someting
    Signed-off-by: khantanvir <aiub.tanvir@gmail.com>

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -33,11 +33,11 @@
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
-                                    <th scope="col">Title33</th>
-                                    <th scope="col">Description55</th>
-                                    <th scope="col">Category66</th>
-                                    <th scope="col">Publish Date77</th>
-                                    <th scope="col">Status33</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Publish Date</th>
+                                    <th scope="col">Status</th>
                                     <th scope="col">Actionttt</th>
                                 </tr>
                             </thead>
